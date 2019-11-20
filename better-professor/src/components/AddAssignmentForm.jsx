7c3e7196c6e5a73c9ea6c901c508fdf43834724c
Jacob Washburn <jacobrwashburn@gmail.com>
2019-11-20 15:22:15 -0700

    styled addAssignment form

--- better-professor/src/components/AddAssignmentForm.jsx
+++ better-professor/src/components/AddAssignmentForm.jsx
@@ -6,18 +6,29 @@ const Form = styled.form`
 `
 
 const Input = styled.input`
+   width: 50%;
+   margin: 0 auto;
+   font-size: 1.5rem;
 `
 
 const InputWrapper = styled.div`
-    max-width: 400px;
-    margin: 0 auto;
+    max-width: 700px;
+    margin: 15% auto;
+    height: 20rem;
     display:flex;
     flex-direction: column;
-    border: 1px solid red;
+    justify-items: center;
+    border: 3px solid blue;
+    border-radius: 2rem;
+    padding: 2rem;
+
 `
 
 const Label = styled.label`
-    margin: 2% 0;
+    margin: 2% auto;
+    font-size: 2.5rem;
+    width: 60%;
+    text-align:center;
 `
 
 const SubmitButton = styled.button`
@@ -25,18 +36,18 @@ const SubmitButton = styled.button`
     margin: 2% auto;
     color: white;
     background-color: blue;
+    font-size: 1.5rem;
 `
 
 const AddAssignment = (props) => {
 
    const id = props.match.params.id
-   console.log('testing', id);
    const [add, setAdd] = useState({
       project_name: '',
       deadline: '',
       student_id: id
    })
-   
+
 
    const handleChange = event => {
       setAdd({ ...add, [event.target.name]: event.target.value })
@@ -47,7 +58,8 @@ const AddAssignment = (props) => {
       axiosWithAuth()
          .post(`https://better-professor-back-end.herokuapp.com/projects/`, add)
          .then(response => {
-            console.log('response after adding student', response.data);
+            console.log('response after adding assignment', response.data);
+            props.history.push(`/assignments/${id}`)
          })
    }
 
@@ -55,23 +67,22 @@ const AddAssignment = (props) => {
 
       <Form onSubmit={handleSubmit} key={id}>
          <InputWrapper>
-            <Label>Name of Assignment:
-                <Input
-                  type='text'
-                  name='project_name'
-                  placeholder='Project/Paper Name'
-                  onChange={handleChange}
-               />
-            </Label>
-            <Label>Deadline:
-                    <Input
-                  type='date'
-                  name='deadline'
-                  onChange={handleChange}
-               />
-            </Label>
+            <Label htmlFor='project_name'>Name of Assignment:</Label>
+            <Input
+               required
+               type='text'
+               name='project_name'
+               placeholder='Project/Paper Name'
+               onChange={handleChange}
+            />
+            <Label htmlFor='deadline'>Deadline: </Label>
+            <Input
+               required
+               type='date'
+               name='deadline'
+               onChange={handleChange}
+            />
             <SubmitButton type='submit'>Add Assignment</SubmitButton>
-
          </InputWrapper>
       </Form>
 
