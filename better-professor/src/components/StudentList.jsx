b5a235e4df268e411c36e6b8aa9341ff65923b3a
Jacob Washburn <jacobrwashburn@gmail.com>
2019-11-19 20:46:25 -0700

    a few minor tweeks in places and created navheader and rendered it in app.jsx

--- better-professor/src/components/StudentList.jsx
+++ better-professor/src/components/StudentList.jsx
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
 import axiosWithAuth from '../utils/axiosWithAuth';
-<<<<<<< HEAD
-import { Link } from 'react-router-dom'
-=======
 import StudentCard from "./StudentCard"
->>>>>>> 3d28e896eff3413dd2f53c5324b9f74a1f9498c5
 
 const StudentList = (props) => {
    const [students, setStudents] = useState([])
@@ -20,14 +15,11 @@ const StudentList = (props) => {
             console.log(re)
             setStudents(re.data)
          })
-   }, [props.match.params.id])
+   }, [])
    return (
       <div>{students.map(student => {
          return (
-            <div>
-               <StudentCard {...student} />
-            </div>
-
+            <StudentCard key={student.id} {...student} />
          )
       })}
       </div>
